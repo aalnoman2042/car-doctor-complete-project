7640f6cff27151048e193e40ea3f0635f22ae289
aalnoman <abdullahalnoman2042@gmail.com>
2023-05-13 18:34:39 +0600

    login signUp firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
-import {
- 
-  RouterProvider,
-} from "react-router-dom";
-import router from './routes/Routes.jsx';
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes.jsx";
+import AuthProvider from "./Providers/AuthProvider";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<div className='mx-auto max-w-7xl'>
-<React.StrictMode>
- <RouterProvider router={router} />
-  </React.StrictMode>,
-</div>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="mx-auto max-w-7xl">
+    <React.StrictMode>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </React.StrictMode>
+    ,
+  </div>
+);
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -3,25 +3,27 @@ import img from '../../../src/assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 
-const Login = () => {
+const SignUp = () => {
 
-    const {signIn} = useContext(AuthContext) 
+    const {createUser} = useContext(AuthContext)
 
-const handleLogin = event =>{
-    event.preventDefault()
-    const form = event.taret;
-   
-    const email = form.email.value;
-    const password = form.password.value;
+    const handleSignup = event =>{
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
 
-    signIn(email, password)
-    .then(result => {
-        const user = result.user
-        console.log(user);
-    })
-    .catch(error => console.log(error))
-}
+        console.log(name,  email, password);
 
+        // firebase create user
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user
+            console.log(user);
+        })
+        .catch(error => console.log(error))
+    }
     return (
         <div>
             <div className="min-h-screen hero bg-base-200">
@@ -31,13 +33,19 @@ const handleLogin = event =>{
     </div>
     <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
       <div className="card-body">
-      <h1 className="text-5xl font-bold text-center">Login</h1>
-     <form onSubmit={handleLogin}>
+      <h1 className="text-5xl font-bold text-center">Sign up</h1>
+     <form onSubmit={handleSignup}>
+     <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name='name' placeholder="name" className="input input-bordered" />
+        </div>
      <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" name='email' placeholder="email" className="input input-bordered" />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
@@ -50,10 +58,10 @@ const handleLogin = event =>{
         </div>
         <div className="mt-6 form-control">
      
-          <input  className="btn btn-primary" type="submit" value="Login" />
+          <input  className="btn btn-primary" type="submit" value="Sign up" />
         </div>
      </form>
-     <p className='my-4 text-center'>new to car doctor?<Link to='/signup' className='text-orange-600'>Sign up</Link></p>
+     <p className='my-4 text-center'>already ahve an account?<Link to='/signup' className='text-orange-600'>log in</Link></p>
       </div>
     </div>
   </div>
@@ -62,4 +70,4 @@ const handleLogin = event =>{
     );
 };
 
-export default Login;
+export default SignUp;
